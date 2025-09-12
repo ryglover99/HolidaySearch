7f0472a07c19e278726f11282b92038568dff8e3
ryglover99 <ryanglover123@gmail.com>
2025-09-12 22:31:44 +0100

    Created skeleton tests for JsonDataLoader to test loading flight and hotel json into objects

---
+++ HolidaySearch.Tests/Data/JsonDataLoaderFlightsTests.cs
@@ -0,0 +1,30 @@
+﻿namespace HolidaySearch.Tests.Data
+{
+    public class JsonDataLoaderFlightsTests
+    {
+        [Fact]
+        public void Load_ValidFlightJson_ReturnsFlightObjects()
+        {
+            //Arrange
+            //Act
+            //Assert
+        }
+
+        [Fact]
+        public void Load_EmptyFlightJson_ReturnsEmptyCollectionFlightObjects()
+        {
+            //Arrange
+            //Act
+            //Assert
+        }
+
+        [Fact]
+        public void Load_InvalidFlightJson_ThrowsException()
+        {
+            //Arrange
+            //Act
+            //Assert
+        }
+
+    }
+}
---
+++ HolidaySearch.Tests/Data/JsonDataLoaderHotelTests.cs
@@ -0,0 +1,30 @@
+﻿namespace HolidaySearch.Tests.Data
+{
+    public class JsonDataLoaderHotelTests
+    {
+        [Fact]
+        public void Load_ValidHotelJson_ReturnsHotelObjects()
+        {
+            //Arrange
+            //Act
+            //Assert
+        }
+
+        [Fact]
+        public void Load_EmptyHotelJson_ReturnsEmptyCollectionHotelObjects()
+        {
+            //Arrange
+            //Act
+            //Assert
+        }
+
+        [Fact]
+        public void Load_InvalidHotelJson_ThrowsException()
+        {
+            //Arrange
+            //Act
+            //Assert
+        }
+
+    }
+}
