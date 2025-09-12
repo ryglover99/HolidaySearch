02458c48e52d3309f4e70473b9b24227cbbeb5d1
ryglover99 <ryanglover123@gmail.com>
2025-09-12 22:01:39 +0100

    Created POCO models for flight + hotel

---
+++ HolidaySearch/Models/Flight.cs
@@ -0,0 +1,13 @@
+﻿namespace HolidaySearch.Models
+{
+    public class Flight
+    {
+        public int Id { get; set; }
+        public string Airline { get; set; } = string.Empty;
+        public string From { get; set; } = string.Empty;
+        public string To { get; set; } = string.Empty;
+        public int Price { get; set; }
+        public DateOnly DepartureDate { get; set; }
+
+    }
+}
---
+++ HolidaySearch/Models/Hotel.cs
@@ -0,0 +1,12 @@
+﻿namespace HolidaySearch.Models
+{
+    public class Hotel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public DateOnly ArrivalDate { get; set; }
+        public int PricePerNight { get; set; }
+        public List<string> LocalAirports { get; set; } = [];
+        public int Nights { get; set; }
+    }
+}
