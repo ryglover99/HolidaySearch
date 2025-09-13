1a760938034d62436234d6e0cebcd08bf2f213c8
ryglover99 <ryanglover123@gmail.com>
2025-09-13 19:15:48 +0100

    Created first failing tests in JsonDataLoaderFlightTests

--- HolidaySearch.Tests/Data/JsonDataLoaderFlightsTests.cs
+++ HolidaySearch.Tests/Data/JsonDataLoaderFlightsTests.cs
@@ -1,29 +1,52 @@
-﻿namespace HolidaySearch.Tests.Data
+﻿using Newtonsoft.Json;
+
+namespace HolidaySearch.Tests.Data
 {
     public class JsonDataLoaderFlightsTests
     {
         [Fact]
-        public void Load_ValidFlightJson_ReturnsFlightObjects()
+        public async Task Load_ValidFlightJson_ReturnsFlightObjects()
         {
             //Arrange
+            string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "flights.json");
+            string flightJson = await File.ReadAllTextAsync(filePath);
+
             //Act
+            var parsedFlights = JsonConvert.DeserializeObject<List<Flight>>(flightJson);
+
             //Assert
+            Assert.NotNull(parsedFlights);
+            Assert.NotEmpty(parsedFlights);
+            Assert.IsType<List<Flight>>(parsedFlights);
+            Assert.All(parsedFlights, f => Assert.IsType<Flight>(f));
         }
 
         [Fact]
-        public void Load_EmptyFlightJson_ReturnsEmptyCollectionFlightObjects()
+        public async Task Load_EmptyFlightJson_ReturnsEmptyCollectionFlightObjects()
         {
             //Arrange
+            string emptyFlightJson = "[]";
+
             //Act
+            var parsedFlights = JsonConvert.DeserializeObject<List<Flight>>(emptyFlightJson);
+
             //Assert
+            Assert.NotNull(parsedFlights);
+            Assert.IsType<List<Flight>>(parsedFlights);
+            Assert.Empty(parsedFlights);
         }
 
         [Fact]
         public void Load_InvalidFlightJson_ThrowsException()
         {
             //Arrange
-            //Act
-            //Assert
+            string invalidFlightJson = "[{ not valid }]";
+
+            //Act & Assert
+            Assert.Throws<JsonReaderException>(() =>
+            {
+                JsonConvert.DeserializeObject<List<Flight>>(invalidFlightJson);
+            });
         }
 
     }
