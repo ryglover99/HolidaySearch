0d5d1377de1284508ce4601e3875ecd69a3e74fa
ryglover99 <ryanglover123@gmail.com>
2025-09-14 18:51:01 +0100

    WIP: Created HolidaySearchService and amended HolidaySearchServiceTests to implement it. Currently the tests fail as first place Best Value holiday does not match up with expected value. Added test fight + hotel data to run tests against

--- HolidaySearch.Tests/Services/HolidaySearchServiceTests.cs
+++ HolidaySearch.Tests/Services/HolidaySearchServiceTests.cs
@@ -1,4 +1,7 @@
-﻿namespace HolidaySearch.Tests.Services
+﻿using HolidaySearch.Models;
+using HolidaySearch.Services;
+
+namespace HolidaySearch.Tests.Services
 {
     public class HolidaySearchServiceTests
     {
@@ -6,147 +9,164 @@
         public async Task Search_ManchesterToMalaga_July7_ReturnsResultsWithBestValueHotelAndFlight()
         {
             //Arrange
-            var holidaySearchService = new HolidaySearchService();
+            var jsonDataLoader = new JsonDataLoaderService();
+            var holidaySearchService = new HolidaySearchService(jsonDataLoader);
 
             //Act
-            var results = await holidaySearchService.SearchAsync({
-                DepartingFrom: "MAN",
-                TravelingTo: "AGP",
-                DepartureDate: DateOnly.Parse("2023/07/01"),
-                Duration: 7
+            var results = await holidaySearchService.SearchAsync(new HolidaySearchRequest()
+            {
+                DepartingFrom = "MAN",
+                TravelingTo = "AGP",
+                DepartureDate = DateOnly.Parse("2023/07/01"),
+                Duration = 7
             });
 
             //Assert
             Assert.NotNull(results);
-            Assert.NotEmpty(results);
-            Assert.IsType<List<HolidayResult>>(results);
+            Assert.IsType<HolidaySearchResponse>(results);
+            Assert.NotNull(results.HolidayPackages);
+            Assert.IsAssignableFrom<IEnumerable<HolidayPackage>>(results.HolidayPackages);
+            Assert.NotEmpty(results.HolidayPackages);
 
-            var firstResult = results.First();
+            var firstResult = results.HolidayPackages.First();
             Assert.Equal(2, firstResult.Flight.Id);
-            Assert.Equal("MAN", firstResult.Flight.DepartingFrom);
-            Assert.Equal("AGP", firstResult.Flight.TravelingTo);
+            Assert.Equal("MAN", firstResult.Flight.From);
+            Assert.Equal("AGP", firstResult.Flight.To);
             Assert.True(firstResult.Flight.Price > 0);
 
             Assert.Equal(9, firstResult.Hotel.Id);
-            Assert.True(firstResult.Hotel.Price > 0);
+            Assert.True(firstResult.Hotel.PricePerNight > 0);
             Assert.False(string.IsNullOrWhiteSpace(firstResult.Hotel.Name));
         }
 
         [Fact]
-        public void Search_LondonToMallorca_June15_ReturnsResultsWithBestValueHotelAndFlight()
+        public async Task Search_LondonToMallorca_June15_ReturnsResultsWithBestValueHotelAndFlight()
         {
             //Arrange
-            var holidaySearchService = new HolidaySearchService();
+            var jsonDataLoader = new JsonDataLoaderService();
+            var holidaySearchService = new HolidaySearchService(jsonDataLoader);
 
             //Act
-            var results = await holidaySearchService.SearchAsync({
-                DepartingFrom: "LTN",
-                TravelingTo: "PMI",
-                DepartureDate: DateOnly.Parse("2023/06/15"),
-                Duration: 10
+            var results = await holidaySearchService.SearchAsync(new HolidaySearchRequest(){
+                DepartingFrom = "LTN",
+                TravelingTo = "PMI",
+                DepartureDate = DateOnly.Parse("2023/06/15"),
+                Duration = 10
             });
 
             //Assert
             Assert.NotNull(results);
-            Assert.NotEmpty(results);
-            Assert.IsType<List<HolidayResult>>(results);
+            Assert.IsType<HolidaySearchResponse>(results);
+            Assert.NotNull(results.HolidayPackages);
+            Assert.IsAssignableFrom<IEnumerable<HolidayPackage>>(results.HolidayPackages);
+            Assert.NotEmpty(results.HolidayPackages);
 
-            var firstResult = results.First();
+            var firstResult = results.HolidayPackages.First();
             Assert.Equal(6, firstResult.Flight.Id);
-            Assert.Equal("LTN", firstResult.Flight.DepartingFrom);
-            Assert.Equal("PMI", firstResult.Flight.TravelingTo);
+            Assert.Equal("LTN", firstResult.Flight.From);
+            Assert.Equal("PMI", firstResult.Flight.To);
             Assert.True(firstResult.Flight.Price > 0);
 
             Assert.Equal(5, firstResult.Hotel.Id);
-            Assert.True(firstResult.Hotel.Price > 0);
+            Assert.True(firstResult.Hotel.PricePerNight > 0);
             Assert.False(string.IsNullOrWhiteSpace(firstResult.Hotel.Name));
         }
 
         [Fact]
-        public void Search_AnywhereToGranCanaria_Nov10_ReturnsResultsWithBestValueHotelAndFlight()
+        public async Task Search_AnywhereToGranCanaria_Nov10_ReturnsResultsWithBestValueHotelAndFlight()
         {
             //Arrange
-            var holidaySearchService = new HolidaySearchService();
+            var jsonDataLoader = new JsonDataLoaderService();
+            var holidaySearchService = new HolidaySearchService(jsonDataLoader);
 
             //Act
-            var results = await holidaySearchService.SearchAsync({
-                DepartingFrom: "",
-                TravelingTo: "LPA",
-                DepartureDate: DateOnly.Parse("2022/11/10"),
-                Duration: 14
+            var results = await holidaySearchService.SearchAsync(new HolidaySearchRequest(){
+                DepartingFrom = "",
+                TravelingTo = "LPA",
+                DepartureDate = DateOnly.Parse("2022/11/10"),
+                Duration = 14
             });
 
             //Assert
             Assert.NotNull(results);
-            Assert.NotEmpty(results);
-            Assert.IsType<List<HolidayResult>>(results);
+            Assert.IsType<HolidaySearchResponse>(results);
+            Assert.NotNull(results.HolidayPackages);
+            Assert.IsAssignableFrom<IEnumerable<HolidayPackage>>(results.HolidayPackages);
+            Assert.NotEmpty(results.HolidayPackages);
 
-            var firstResult = results.First();
+            var firstResult = results.HolidayPackages.First();
             Assert.Equal(7, firstResult.Flight.Id);
-            Assert.Equal("LPA", firstResult.Flight.TravelingTo);
+            Assert.Equal("LPA", firstResult.Flight.To);
             Assert.True(firstResult.Flight.Price > 0);
 
             Assert.Equal(6, firstResult.Hotel.Id);
-            Assert.True(firstResult.Hotel.Price > 0);
+            Assert.True(firstResult.Hotel.PricePerNight > 0);
             Assert.False(string.IsNullOrWhiteSpace(firstResult.Hotel.Name));
         }
 
         [Fact]
-        public void Search_WithNoMatchingFlights_ReturnEmptyResults()
+        public async Task Search_WithNoMatchingFlights_ReturnEmptyResults()
         {
             //Arrange
-            var holidaySearchService = new HolidaySearchService();
+            var jsonDataLoader = new JsonDataLoaderService();
+            var holidaySearchService = new HolidaySearchService(jsonDataLoader);
 
             //Act
-            var results = await holidaySearchService.SearchAsync({
-                DepartingFrom: "MAN",
-                TravelingTo: "LTN",
-                DepartureDate: DateOnly.Parse("2022/11/10"),
-                Duration: 6
+            var results = await holidaySearchService.SearchAsync(new HolidaySearchRequest(){
+                DepartingFrom = "MAN",
+                TravelingTo = "LTN",
+                DepartureDate = DateOnly.Parse("2022/11/10"),
+                Duration = 6
             });
 
             //Assert
             Assert.NotNull(results);
-            Assert.Empty(results);
-            Assert.IsType<List<HolidayResult>>(results);
+            Assert.IsType<HolidaySearchResponse>(results);
+            Assert.NotNull(results.HolidayPackages);
+            Assert.IsAssignableFrom<IEnumerable<HolidayPackage>>(results.HolidayPackages);
+            Assert.NotEmpty(results.HolidayPackages);
         }
 
         [Fact]
-        public void Search_WithEmptySearchInput_ThrowNullArgumentException()
+        public async Task Search_WithEmptySearchInput_ThrowNullArgumentException()
         {
             //Arrange
-            var holidaySearchService = new HolidaySearchService();
+            var jsonDataLoader = new JsonDataLoaderService();
+            var holidaySearchService = new HolidaySearchService(jsonDataLoader);
 
             //Act & Assert
-            Assert.Throws<ArgumentNullException>(() =>
+            await Assert.ThrowsAsync<ArgumentNullException>(async () =>
             {
-                var results = await holidaySearchService.SearchAsync({
-                    DepartingFrom: null,
-                    TravelingTo: null,
-                    DepartureDate: null,
-                    Duration: 0
+                var results = await holidaySearchService.SearchAsync(new HolidaySearchRequest(){
+                    DepartingFrom = null,
+                    TravelingTo = null,
+                    DepartureDate = DateOnly.Parse("2022/11/10"),
+                    Duration = 0
                 });
             });
         }
 
         [Fact]
-        public void Search_WithDurationZero_ReturnsEmptyResults()
+        public async Task Search_WithDurationZero_ReturnsEmptyResults()
         {
             //Arrange
-            var holidaySearchService = new HolidaySearchService();
+            var jsonDataLoader = new JsonDataLoaderService();
+            var holidaySearchService = new HolidaySearchService(jsonDataLoader);
 
             //Act
-            var results = await holidaySearchService.SearchAsync({
-                DepartingFrom: "MAN",
-                TravelingTo: "LTN",
-                DepartureDate: DateOnly.Parse("2022/11/10"),
-                Duration: 0
+            var results = await holidaySearchService.SearchAsync(new HolidaySearchRequest(){
+                DepartingFrom = "MAN",
+                TravelingTo = "LTN",
+                DepartureDate = DateOnly.Parse("2022/11/10"),
+                Duration = 0
             });
 
             //Assert
             Assert.NotNull(results);
-            Assert.Empty(results);
-            Assert.IsType<List<HolidayResult>>(results);
+            Assert.IsType<HolidaySearchResponse>(results);
+            Assert.NotNull(results.HolidayPackages);
+            Assert.IsAssignableFrom<IEnumerable<HolidayPackage>>(results.HolidayPackages);
+            Assert.NotEmpty(results.HolidayPackages);
         }
 
     }
---
+++ HolidaySearch/Services/HolidaySearchService.cs
@@ -0,0 +1,68 @@
+﻿using HolidaySearch.Models;
+using HolidaySearch.Services.Interfaces;
+
+namespace HolidaySearch.Services
+{
+    public class HolidaySearchService : IHolidaySearchService
+    {
+        private readonly IJsonDataLoaderService _jsonDataLoaderService;
+        private const string FLIGHT_DATA_PATH = "Data/flights.json";
+        private const string HOTEL_DATA_PATH = "Data/hotels.json";
+
+        public HolidaySearchService(IJsonDataLoaderService jsonDataLoaderService) 
+        {
+            _jsonDataLoaderService = jsonDataLoaderService;
+        }
+
+        private async Task<List<Flight>> LoadAllFlights()
+            => await _jsonDataLoaderService.LoadAllAsync<Flight>(FLIGHT_DATA_PATH);
+        private async Task<List<Hotel>> LoadAllHotels()
+            => await _jsonDataLoaderService.LoadAllAsync<Hotel>(HOTEL_DATA_PATH);
+
+        public async Task<HolidaySearchResponse> SearchAsync(HolidaySearchRequest request)
+        {
+            HolidaySearchResponse response = new HolidaySearchResponse();
+
+            var flightsTask = LoadAllFlights();
+            var hotelsTask = LoadAllHotels();
+
+            await Task.WhenAll(flightsTask, hotelsTask);
+
+            var flights = await flightsTask;
+            var hotels = await hotelsTask;
+
+            //Search Algorithm
+
+            List<HolidayPackage> packageHolidays = [];
+
+            foreach (var flight in flights)
+            {
+                //Check for valid flight
+                if (flight.From != request.DepartingFrom) continue;
+                if (flight.To != request.TravelingTo) continue;
+                if (flight.DepartureDate < request.DepartureDate) continue;
+
+                foreach (var hotel in hotels)
+                {
+                    //Check for valid hotel
+                    if (!hotel.LocalAirports.Contains(flight.To)) continue;
+                    if (hotel.ArrivalDate < request.DepartureDate) continue;
+                    if (hotel.Nights != request.Duration) continue;
+
+                    packageHolidays.Add(new HolidayPackage()
+                    {
+                        Flight = flight,
+                        Hotel = hotel,
+                        TotalPrice = flight.Price + (hotel.PricePerNight * request.Duration)
+                    });
+                }
+            }
+
+            response.HolidayPackages = packageHolidays.OrderBy(p => p.TotalPrice);
+
+            return response;
+
+        }
+      
+    }
+}
